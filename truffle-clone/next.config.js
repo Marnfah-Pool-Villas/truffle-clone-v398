a70d60226a678e1041a48205eb3eb1612ea96771
Marnfah-Pool-Villas <icannotsee3@gmail.com>
2025-10-02 16:00:51 +0700

    Update next.config.js

--- truffle-clone/next.config.js
+++ truffle-clone/next.config.js
@@ -1,7 +1,12 @@
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  allowedDevOrigins: ['builder.io', 'https://builder.io', '*.fly.dev', 'e5fdd11587534640ab84596faaf47bce-2e1a48e0-0808-4133-9129-94138a.fly.dev', 'https://e5fdd11587534640ab84596faaf47bce-2e1a48e0-0808-4133-9129-94138a.fly.dev'],
+  reactStrictMode: true,
+  allowedDevOrigins: [
+    'builder.io',
+    'https://builder.io',
+    '*.fly.dev',
+    'e5fdd11587534640ab84596faaf47bce-2e1a48e0-0808-4133-9129-94138a.fly.dev',
+    'https://e5fdd11587534640ab84596faaf47bce-2e1a48e0-0808-4133-9129-94138a.fly.dev',
+  ],
   images: {
     unoptimized: true,
     domains: [
@@ -11,26 +16,10 @@ const nextConfig = {
       "ugc.same-assets.com",
     ],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "source.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "ext.same-assets.com", pathname: "/**" },
+      { protocol: "https", hostname: "ugc.same-assets.com", pathname: "/**" },
     ],
   },
 };
